0b0b03f1241e337d8d6ad9bfd233e64b0282ac53
Aurelia F. Simatupang <dorm4ever@gmail.com>
2023-11-27 17:36:22 +0100

    smsall word fix

--- src/App.js
+++ src/App.js
@@ -31,31 +31,8 @@ function App() {
         </div>
         <br />
         <br />
-        <p class="footer-text">Hosted by Netlify and <a href="https://github.com/aurelreynasimatupang/aurel-reyna">GitHub</a> </p>
-      
+        <p class="footer-text">Built with React. Hosted by Netlify and <a href="https://github.com/aurelreynasimatupang/aurel-reyna">GitHub</a> </p>
       </div>
-      {/* <div class="main">
-        <div class="content">
-          <br />
-          <br />
-          <Main />
-        </div>
-        <div id="about" class="content">
-          <br />
-          <br />
-          <About />
-        </div>
-        <div id="portfolio" class="content">
-          <br />
-          <br />
-          <Portfolio />
-        </div>
-        <div id="contact" class="content">
-          <br />
-          <Contact />
-        </div>
-        <p class="footer-text">Hosted by Netlify and <a href="https://github.com/aurelreynasimatupang/aurel-reyna">GitHub</a> </p>
-      </div> */}
     </div>
   );
 }
