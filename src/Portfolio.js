5400740406c999dd9bdde5b6b798329acd47587e
Aurelia F. Simatupang <dorm4ever@gmail.com>
2023-12-05 17:23:05 +0100

    added portfolio

--- src/Portfolio.js
+++ src/Portfolio.js
@@ -4,31 +4,39 @@ import Card from "./Card";
 
 function Portfolio(props) {
     const portfolio = [
-        {
-          title: 'Weather App',
-          text: 'Project as part of the SheCodes React workshop, built with React.js and axios on top of free API',
-          link: 'https://aurelreynashecodesreact.netlify.app/',
-          img_url:'./img/weather.png',
-          img_alt:'Weather App',
-          img_title:'Weather App created by Aurelia Simatupang'
-        },
-        {
-          title: 'Dictionary App',
-          text: 'Project as part of the SheCodes React workshop, built with React.js and axios on top of free API',
-          link: 'https://reverent-lalande-d8625c.netlify.app/',
-          img_url:'./img/dictionary.png',
-          img_alt:'Dictionary App',
-          img_title:'Dictionary App created by Aurelia Simatupang'
-        },
-        {
-          title: 'Article CMS',
-          text: '2.5 months internship with Siloam Hospital, code follows a Figma design. Vue.js front-end and Node.js backend, including commercial article website and the article CMS interface for authors.',
-          link: 'https://www.siloamhospitals.com/informasi-siloam/artikel/',
-          img_url:'./img/article.png',
-          img_alt:'Article CMS',
-          img_title:'Internship work by Aurelia Simatupang'
-        }
-      ];
+      {
+        title: 'Article CMS',
+        text: '2.5 months internship with Siloam Hospital, code follows a Figma design. Vue.js front-end and Node.js backend, including commercial article website and the article CMS interface for authors.',
+        link: 'https://www.siloamhospitals.com/informasi-siloam/artikel/',
+        img_url:'./img/article.png',
+        img_alt:'Article CMS',
+        img_title:'Internship work by Aurelia Simatupang'
+      },
+      {
+        title: `Animation student's portfolio`,
+        text: 'A responsive portfolio website designed for an animation student, with animations and advanced JavaScript functions.',
+        link: 'https://louisashannontara.netlify.app/',
+        img_url:'./img/shannon.png',
+        img_alt:'Article CMS',
+        img_title:'Oortfolio website by Aurelia Simatupang'
+      },
+      {
+        title: 'Weather App',
+        text: 'Project as part of the SheCodes React workshop, built with React.js and axios on top of free API',
+        link: 'https://aurelreynashecodesreact.netlify.app/',
+        img_url:'./img/weather.png',
+        img_alt:'Weather App',
+        img_title:'Weather App created by Aurelia Simatupang'
+      },
+      {
+        title: 'Dictionary App',
+        text: 'Project as part of the SheCodes React workshop, built with React.js and axios on top of free API',
+        link: 'https://reverent-lalande-d8625c.netlify.app/',
+        img_url:'./img/dictionary.png',
+        img_alt:'Dictionary App',
+        img_title:'Dictionary App created by Aurelia Simatupang'
+      }
+    ];
     return (
         <div>
             <h2 class="text-center">Portfolio</h2>
